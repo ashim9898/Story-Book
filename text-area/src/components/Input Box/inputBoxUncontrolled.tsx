d831a905c3c5e70f15d0af01de63aec5fe4d4650
ashimpoudyal98 <75020083+ashimpoudyal@users.noreply.github.com>
2023-11-27 16:14:39 +0545

    Form Implementation with Node

--- text-area/src/components/Input Box/inputBoxUncontrolled.tsx
+++ text-area/src/components/Input Box/inputBoxUncontrolled.tsx
@@ -1,36 +1,65 @@
+import { useState, useEffect } from 'react';
 import { Input } from '@chakra-ui/react';
 import { useInputBox } from './useInputBox';
 
+interface FormField {
+    label: string;
+    type: string;
+}
+
 export const InputBoxUncontrolled = (props: any) => {
-  const { onChangeRHF, value: rhfValue, onChange, ...propsRest } = props;
-  const {
-    name,
-    value,
-    label,
-    control,
-    errors,
-    required,
-    rule,
-    onChange: _onChange,
-    ...contextRest
-  } = useInputBox();
+    const { onChangeRHF,value: rhfValue, onChange, ...propsRest } = props;
+    const {
+        name,
+        control,
+        errors,
+        required,
+        rule,
+        onChange: _onChange,
+        ...contextRest
+    } = useInputBox();
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {value} = event.target
-    _onChange?.(name, value);
-    onChangeRHF?.(value);
-  };
+    const [formTemplate, setFormTemplate] = useState<FormField[]>([]);
 
-  const valueNormalized = rhfValue ?? value;
+    const [values, setValues] = useState<{ [key: string]: string }>({});
 
-  const inputProps = {
-    name,
-    value: valueNormalized,
-    ...contextRest,
-    ...propsRest,
-  };
-  return (
-    <Input placeholder='Basic usage' onChange={handleChange} {...inputProps}/>
- 
-  );
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setValues({ ...values, [name]: value });
+        _onChange?.(name, value);
+        onChangeRHF?.(value);
+    };
+
+    const inputProps = {
+        ...contextRest,
+        ...propsRest,
+    };
+
+    useEffect(() => {
+        // Fetch the form template from the server
+        fetch('http://localhost:4000/formTemplate.txt')
+            .then(response => response.text())
+            .then(data => {
+                setFormTemplate(JSON.parse(data) as FormField[]);
+            });
+    }, []);
+
+    return (
+        <div>
+            {formTemplate.map((field, index) => (
+                <div key={index}>
+                    <label>{field.label}</label>
+                    <Input
+                        type={field.type}
+                        name={field.label}
+                        value={values[field.label] ?? ''}
+                        onChange={handleChange}
+                        {...inputProps}
+                        placeholder={field.label}
+                    />
+                    
+                </div>
+            ))}
+        </div>
+    );
 };
---
+++ text-area/src/components/form.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 
+
+const form = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default form
